1bd378f375b033c314d0d5257192992a4231a39b
Dipanshu Zalke <141048512+deecoding07@users.noreply.github.com>
2024-04-21 23:05:40 +0530

    Add files via upload

---
+++ Day 1/app.js
@@ -0,0 +1,53 @@
+//Method 1
+let str1 = "100 Day Of Javascript Code Challenge";
+
+function findLongestWord1(str1) {
+  let arr = str1.split(" ");
+  let max = arr[0];
+  for(let i = 0; i<arr.length; i++){
+    if(max.length<arr[i].length) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+console.log(findLongestWord1(str1));
+
+// OR
+
+//Method2
+let str2 = "100 Day Of Javascript Code Challenge";
+
+const findLongestWord2 = (str2) => {
+  if (str.trim().length === 0) {
+    return false;
+  }
+
+  words = str.split(" ");
+  words = words.sort((a, b) => b.length - a.length); //Comparison function
+  return words[0];
+};
+
+console.log(findLongestWord2(str2));
+
+// OR 
+
+//Method3
+let str3 = "100 Day Of Javascript Code Challenge";
+
+function giveBigString(str2) {
+  let val = 0;
+  let string = "";
+  let arr = str2.split(" ");
+
+  arr.map((e) => {
+    if(e.length > val) {
+      string = e;
+      val = e.length;
+    }
+  });
+  return string;
+}
+
+console.log(giveBigString(str3));
