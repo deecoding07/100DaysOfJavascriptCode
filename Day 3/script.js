b3d8fef924d45794107703c4d883aa8cc5332680
Dipanshu Zalke <141048512+deecoding07@users.noreply.github.com>
2024-04-24 00:57:02 +0530

    Add files via upload

---
+++ Day 3/script.js
@@ -0,0 +1,51 @@
+//Method1
+const countChar = (word,char) => {
+    word = word.toLowerCase();
+    char = char.toLowerCase();
+
+    totalCount = word.split("").reduce((res, el) => {
+        if (el === char) {
+            res++;
+        }
+        return res;
+    }, 0);
+    return totalCount;
+}
+console.log(countChar("Computer Science", "c"));
+
+// OR
+
+//Method2
+const countChar2 = (word, char) => {
+    word = word.toLowerCase();
+    char = char.toLowerCase();
+    
+    let count = 0;
+    for(let i=0; i<word.length; i++){
+        if(word[i] === char){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countChar2("Computer Science", "c"));
+
+//OR 
+
+//Method3
+const countChar3 = (word, char) => {
+    word = word.toLowerCase();
+    char = char.toLowerCase();
+    
+    let count = 0;
+    for(const letter of word){
+        if(char === letter){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countChar3("Computer Science", "c"));
+
+
+//Ans.= 4
